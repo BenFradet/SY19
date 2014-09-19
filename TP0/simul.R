257cb106e5f00ef8de148f446423bd18fa7d3dad
BenFradet <benjamin.fradet@gmail.com>
2014-09-19 20:05:16 +0200

    moved away from many binomial distributions to a single multinomial distribution

--- TP0/simul.R
+++ TP0/simul.R
@@ -1,10 +1,10 @@
 library(MASS)
 
-simul <- function(n, pi1, pi2, pi3, mu1, mu2, mu3, sigma1, sigma2, sigma3) {
-    #replace rbinom by multinomial
-    n1 <- rbinom(1, n, pi1)
-    n2 <- rbinom(1, n, pi2)
-    n3 <- n - n1 - n2
+simul <- function(n, pis, mu1, mu2, mu3, sigma1, sigma2, sigma3) {
+    multinomial <- rmultinom(1, size = n, prob = pis)
+    n1 <- multinomial[1,1]
+    n2 <- multinomial[2,1]
+    n3 <- multinomial[3,1]
 
     d1 <- mvrnorm(n1, mu1, sigma1)
     d1 <- cbind(d1, matrix(1, nrow = n1))
