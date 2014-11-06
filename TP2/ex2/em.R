536f3673d169daf8c69d66a38a8b1c5c8d478944
BenFradet <benjamin.fradet@gmail.com>
2014-11-06 09:56:18 +0100

    em function

--- TP2/ex2/em.R
+++ TP2/ex2/em.R
@@ -1,66 +1,96 @@
-source('gaussianDensity.R')
+source('gaussian.R')
 
-n <- 1000
-n1 <- rbinom(1, n, 0.5)
-n1Plus1 <- n1 + 1
-x <- c(rnorm(n1), rnorm(n - n1, mean = 6, sd = 5))
+em <- function(x, nbClasses) {
 
-# Expectation-maximization algorithm
-# computation of the initial parameters
-pis <- matrix(0, nrow = n, ncol = 2)
-pis[1, 1] <- n1 / n
-pis[1, 2] <- (n - n1) / n
+    n <- length(x)
 
-mus <- matrix(0, nrow = n, ncol = 2)
-mus[1, 1] <- mean(x[1:n1])
-mus[1, 2] <- mean(x[n1Plus1:n])
+    # standardize the input data
+    x <- x - mean(x)
+    x <- x / sd(x)
 
-sigmas <- matrix(0, nrow = n, ncol = 2)
-sigmas[1, 1] <- sd(x[1:n1]) ^ 2
-sigmas[1, 2] <- sd(x[n1Plus1:n]) ^ 2
+    # classification à partir des tik de la dernière itération?
+    # sensible aux outliers
+    # faire 100 fois et prendre la solution avec la plus grande vraisemblance
 
-t <- matrix(0, nrow = n, ncol = 2)
-iter <- 0
-stoppingCriterion <- 10^-16
+    # Expectation-maximization algorithm
+    # computation of the initial parameters
 
-repeat {
-    # expectation step, computation of the t_ik
-    iter <- iter + 1
-    for (i in 1:n) {
-        den <- pis[iter, 1] *
-                gaussianDensity(x[i], mus[iter, 1], sigmas[iter, 1]) +
-            pis[iter, 2] * gaussianDensity(x[i], mus[iter, 2], sigmas[iter, 2])
-        t[i, 1] <- pis[iter, 1] *
-            gaussianDensity(x[i], mus[iter, 1], sigmas[iter, 1]) / den
-        t[i, 2] <- pis[iter, 2] *
-            gaussianDensity(x[i], mus[iter, 2], sigmas[iter, 2]) / den
-    }
+    # 1 / k (k = # nb of classes)
+    pis <- matrix(0, nrow = n, ncol = 2)
+    pis[1, 1] <- 1 / nbClasses
+    pis[1, 2] <- 1 / nbClasses
 
-    # maximization step, update of the parameters
-    pis[iter + 1, 1] <- sum(t[1:n, 1]) / n
-    pis[iter + 1, 2] <- 1 - pis[iter + 1, 1]
-    mus[iter + 1, 1] <- sum(t[1:n, 1] * x[1:n]) / sum(t[1:n, 1])
-    mus[iter + 1, 2] <- sum(t[1:n, 2] * x[1:n]) / sum(t[1:n, 2])
-    sigmas[iter + 1, 1] <- sum(t[1:n, 1] * (x[1:n] - mus[iter + 1, 1]) ^ 2) /
-        sum(t[1:n, 1])
-    sigmas[iter + 1, 2] <- sum(t[1:n, 2] * (x[1:n] - mus[iter + 1, 2]) ^ 2) /
-        sum(t[1:n, 2])
+    # random values between -1 and 1 because x centered
+    mus <- matrix(0, nrow = n, ncol = 2)
+    mus[1, 1] <- runif(1, -1, 1)
+    print(mus[1, 1])
+    mus[1, 2] <- runif(1, -1, 1)
+    print(mus[1,2])
 
-    # check for convergence
-    #diff <- sqrt(abs(pis[iter, 1] - pis[iter + 1, 1]) +
-    #          abs(pis[iter, 2] - pis[iter + 1, 2]) +
-    #          abs(mus[iter, 1] - mus[iter + 1, 1]) +
-    #          abs(mus[iter, 2] - mus[iter + 1, 2]) +
-    #          abs(sigmas[iter, 1] - sigmas[iter + 1, 1]) +
-    #          abs(sigmas[iter, 2] - sigmas[iter + 1, 2]))
-    diff <- sqrt(abs(pis[iter, 1] - pis[iter + 1, 1]) +
-                 abs(pis[iter, 2] - pis[iter + 1, 2]))
-    if (diff < stoppingCriterion) {
-        cat('Converged in', iter, 'iterations\n')
-        cat('pi1 =', pis[iter + 1, 1], 'pi2 =', pis[iter + 1, 2], '\n')
-        cat('mu1 =', mus[iter + 1, 1], 'mu2 =', mus[iter + 1, 2], '\n')
-        cat('sigma1 =', sigmas[iter + 1, 1], 'sigma2 =', sigmas[iter + 1, 2],
-            '\n')
-        break
+    # initialized to 1 because x standardized
+    sigmas <- matrix(0, nrow = n, ncol = 2)
+    sigmas[1, 1] <- 1
+    sigmas[1, 2] <- 1
+
+    t <- matrix(0, nrow = n, ncol = 2)
+    iter <- 0
+    stoppingCriterion <- 10^-9
+
+    repeat {
+        # expectation step, computation of the t_ik
+        iter <- iter + 1
+        for (i in 1:n) {
+            den <- pis[iter, 1] *
+                    gaussian(x[i], mus[iter, 1], sigmas[iter, 1]) +
+                pis[iter, 2] * gaussian(x[i], mus[iter, 2], sigmas[iter, 2])
+            t[i, 1] <- pis[iter, 1] *
+                gaussian(x[i], mus[iter, 1], sigmas[iter, 1]) / den
+            t[i, 2] <- pis[iter, 2] *
+                gaussian(x[i], mus[iter, 2], sigmas[iter, 2]) / den
+        }
+
+        # maximization step, update of the parameters
+        pis[iter + 1, 1] <- sum(t[1:n, 1]) / n
+        pis[iter + 1, 2] <- 1 - pis[iter + 1, 1]
+        mus[iter + 1, 1] <- sum(t[1:n, 1] * x[1:n]) / sum(t[1:n, 1])
+        mus[iter + 1, 2] <- sum(t[1:n, 2] * x[1:n]) / sum(t[1:n, 2])
+        sigmas[iter + 1, 1] <- sum(t[1:n, 1] * (x[1:n] -
+            mus[iter + 1, 1]) ^ 2) / sum(t[1:n, 1])
+        sigmas[iter + 1, 2] <- sum(t[1:n, 2] * (x[1:n] -
+            mus[iter + 1, 2]) ^ 2) / sum(t[1:n, 2])
+
+        # check for convergence
+        likelihood <- 0
+        diff <- abs(pis[iter, 1] - pis[iter + 1, 1]) +
+                abs(pis[iter, 2] - pis[iter + 1, 2]) +
+                abs(mus[iter, 1] - mus[iter + 1, 1]) +
+                abs(mus[iter, 2] - mus[iter + 1, 2]) +
+                abs(sigmas[iter, 1] - sigmas[iter + 1, 1]) +
+                abs(sigmas[iter, 2] - sigmas[iter + 1, 2])
+        if (diff < stoppingCriterion) {
+            # computes the likelihood of our solution
+            for (i in 1:n) {
+                likelihood <- likelihood +
+                    t[i, 1] * log(pis[iter + 1, 1] *
+                        gaussian(x[i], mus[iter + 1, 1], sigmas[iter + 1, 1])) +
+                    t[i, 2] * log(pis[iter + 1, 2] *
+                        gaussian(x[i], mus[iter + 1, 2], sigmas[iter + 1, 2]))
+            }
+
+            res <- NULL
+            res$iter <- iter
+            res$pi1 <- pis[iter + 1, 1]
+            res$pi2 <- pis[iter + 1, 2]
+            res$mu1 <- mus[iter + 1, 1]
+            res$mu2 <- mus[iter + 1, 2]
+            res$sigma1 <- sigmas[iter + 1, 1]
+            res$sigma2 <- sigmas[iter + 1, 2]
+            res$likelihood <- likelihood
+
+            # computes a class vector
+            # res$class <- apply(
+
+            return(res)
+        }
     }
 }
