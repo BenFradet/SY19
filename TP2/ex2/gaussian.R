01b0cfc46fe9e7ca190049de681b38ab4cb320d5
BenFradet <benjamin.fradet@gmail.com>
2014-11-06 09:56:07 +0100

    rename of the gaussian function

--- TP2/ex2/gaussian.R
+++ TP2/ex2/gaussian.R
@@ -1,4 +1,4 @@
 # function to compute the density of a normal distribution at a given point
-gaussianDensity <- function(x, mu, sigma) {
+gaussian <- function(x, mu, sigma) {
     return(1 / sqrt(sigma * 2 * pi) * exp(-(x - mu) ^ 2 / (2 * sigma)))
 }
