e302b5dcba4d49356383df7afeb04553b4bad5b6
BenFradet <benjamin.fradet@gmail.com>
2014-11-13 10:05:03 +0100

    cem in main

--- TP2/ex2/main.R
+++ TP2/ex2/main.R
@@ -1,4 +1,5 @@
 source('em.R')
+source('cem.R')
 source('../randIndex.R')
 
 n <- 1000
@@ -11,8 +12,17 @@ x <- x / sd(x)
 
 xClass <- c(rep(1, n1), rep(2, n - n1))
 
-hashmap <- list()
-df <- data.frame(iter = c(),
+hashmapEM <- list()
+dfEM <- data.frame(iter = c(),
+                 pi1 = c(),
+                 pi2 = c(),
+                 mu1 = c(),
+                 mu2 = c(),
+                 sigma1 = c(),
+                 sigma2 = c(),
+                 likelihood = c())
+hashmapCEM <- list()
+dfCEM <- data.frame(iter = c(),
                  pi1 = c(),
                  pi2 = c(),
                  mu1 = c(),
@@ -22,23 +32,44 @@ df <- data.frame(iter = c(),
                  likelihood = c())
 for (i in 1:10) {
     resEM <- em(x, 2)
-    print(resEM$likelihood)
-    hashmap[[paste(resEM$likelihood)]] <- resEM$class
-    df <- rbind(df, resEM[-length(resEM)])
+    cat('resEM:', resEM$likelihood, '\n')
+    hashmapEM[[paste(resEM$likelihood)]] <- resEM$class
+    dfEM <- rbind(dfEM, resEM[-length(resEM)])
+
+    resCEM <- cem(x, 2)
+    cat('resCEM:', resCEM$likelihood, '\n')
+    hashmapCEM[[paste(resCEM$likelihood)]] <- resCEM$class
+    dfCEM <- rbind(dfCEM, resCEM[-length(resCEM)])
 }
 
-maxLikelihood <- max(df$likelihood)
-rowMaxLh <- df[df$likelihood == maxLikelihood, ]
-classMaxLh <- hashmap[[paste(maxLikelihood)]]
+maxLhEM <- max(dfEM$likelihood)
+maxLhCEM <- max(dfCEM$likelihood)
+rowMaxLhEM <- dfEM[dfEM$likelihood == maxLhEM, ]
 
-cat('Converged in', rowMaxLh$iter, 'iterations\n\n')
-cat('pi1 =', rowMaxLh$pi1, 'pi2 =', rowMaxLh$pi2, '\n')
+rowMaxLhCEM <- dfCEM[dfCEM$likelihood == maxLhCEM, ]
+classMaxLhEM <- hashmapEM[[paste(maxLhEM)]]
+classMaxLhCEM <- hashmapCEM[[paste(maxLhCEM)]]
+
+cat('EM:\n')
+cat('Converged in', rowMaxLhEM$iter, 'iterations\n\n')
+cat('pi1 =', rowMaxLhEM$pi1, 'pi2 =', rowMaxLhEM$pi2, '\n')
 cat('piEmp1 =', n1 / n, 'piEmp2 =', (n - n1) / n, '\n\n')
-cat('mu1 =', rowMaxLh$mu1, 'mu2 =', rowMaxLh$mu2, '\n')
+cat('mu1 =', rowMaxLhEM$mu1, 'mu2 =', rowMaxLhEM$mu2, '\n')
 cat('muEmp1 =', mean(x[1:n1]), 'muEmp2 =', mean(x[n1Plus1:n]), '\n\n')
-cat('sigma1 =', rowMaxLh$sigma1, 'sigma2 =', rowMaxLh$sigma2, '\n')
+cat('sigma1 =', rowMaxLhEM$sigma1, 'sigma2 =', rowMaxLhEM$sigma2, '\n')
 cat('sigmaEmp1 =', sd(x[1:n1]) ^ 2, 'sigmaEmp2 =', sd(x[n1Plus1:n]) ^ 2, '\n\n')
-cat('likelihood =', rowMaxLh$likelihood, '\n')
+cat('likelihood =', rowMaxLhEM$likelihood, '\n')
+randIndex <- randindex(classMaxLhEM, xClass)
+cat('rand index =', randIndex$rate, '\n')
 
-randIndex <- randindex(classMaxLh, xClass)
-cat('rand index:', randIndex$rate, '\n')
+cat('\n\nCEM:\n')
+cat('Converged in', rowMaxLhCEM$iter, 'iterations\n\n')
+cat('pi1 =', rowMaxLhCEM$pi1, 'pi2 =', rowMaxLhCEM$pi2, '\n')
+cat('piEmp1 =', n1 / n, 'piEmp2 =', (n - n1) / n, '\n\n')
+cat('mu1 =', rowMaxLhCEM$mu1, 'mu2 =', rowMaxLhCEM$mu2, '\n')
+cat('muEmp1 =', mean(x[1:n1]), 'muEmp2 =', mean(x[n1Plus1:n]), '\n\n')
+cat('sigma1 =', rowMaxLhCEM$sigma1, 'sigma2 =', rowMaxLhCEM$sigma2, '\n')
+cat('sigmaEmp1 =', sd(x[1:n1]) ^ 2, 'sigmaEmp2 =', sd(x[n1Plus1:n]) ^ 2, '\n\n')
+cat('likelihood =', rowMaxLhCEM$likelihood, '\n')
+randIndex <- randindex(classMaxLhCEM, xClass)
+cat('rand index =', randIndex$rate, '\n')
