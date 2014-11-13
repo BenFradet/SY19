5e02b27e3a6436ab7416d43d554739e0d6aac078
BenFradet <benjamin.fradet@gmail.com>
2014-11-13 10:03:47 +0100

    moved away from gaussian to dnorm

--- TP2/ex2/gaussian.R
+++
@@ -1,4 +0,0 @@
-# function to compute the density of a normal distribution at a given point
-gaussian <- function(x, mu, sigma) {
-    return(1 / sqrt(sigma * 2 * pi) * exp(-(x - mu) ^ 2 / (2 * sigma)))
-}
