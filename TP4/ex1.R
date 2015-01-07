13a12788b843bc11c83e686050abe288b7ef7487
BenFradet <benjamin.fradet@gmail.com>
2015-01-07 18:22:18 +0100

    few modifs in ex1

--- TP4/ex1.R
+++ TP4/ex1.R
@@ -1,9 +1,19 @@
 library(e1071)
 
+# matrice des exemples
 x <- matrix(c(2, 0, 0, 2, -2, 2, -1, 3), nrow = 4, byrow = T)
+# matrices des classes associees a ces exemples
 y <- factor(c(-1, 1, 1, 1))
 df <- data.frame(x1 = x[, 1], x2 = x[, 2], class = y)
+# creation du modele grace a la fonction svm
 model <- svm(class ~ x1 + x2, data = df, scale = F, kernel = 'linear')
+
+# affichage des resultats
+cat('support vectors: ', model$SV, '\n')
+cat('alpha(i) * y(i): ', model$coefs, '\n')
+cat('w: ', t(model$coefs) %*% as.matrix(df[model$index, c(1, 2)]), '\n')
+cat('w0: ', -model$rho, '\n')
+
 pngName <- 'svmEx1.png'
 png(pngName)
 plot(model, df,
@@ -13,7 +23,3 @@ plot(model, df,
      main = 'Régions de décision produites par SVM')
 dev.off()
 cat(pngName, 'sauvegardee\n')
-
-cat('support vectors: ', model$SV, '\n')
-cat('alpha(i) * y(i): ', model$coefs, '\n')
-cat('w: ', t(model$coefs) %*% as.matrix(df[model$index, c(1, 2)]), '\n')
